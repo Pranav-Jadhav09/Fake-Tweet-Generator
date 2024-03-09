5b0b3832f40376545800abc035ca93959d51b97b
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-03-09 09:39:22 +0530

    verified badge eventlistener

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,30 @@
 ("use strict");
+
+// User Input Elements
+const tweetVerified = document.getElementById("tweet-verified");
+
+// All radio buttons
+const verifiedRadios = document.getElementsByName("verified-radio");
+
+// Function
+function toggleVerified() {
+  let choice;
+
+  for (let i = 0; i < verifiedRadios.length; i++) {
+    if (verifiedRadios[i].checked) {
+      choice = verifiedRadios[i].value;
+    }
+  }
+
+  if (choice === "show") {
+    tweetVerified.classList.remove("hide");
+  } else {
+    tweetVerified.classList.add("hide");
+  }
+}
+
+// EventListeners
+
+for (let i = 0; i < verifiedRadios.length; i++) {
+  verifiedRadios[i].addEventListener("change", toggleVerified);
+}
